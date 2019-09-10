5b25cba3b130681cb1094fd3362229055751da38
Galsor <meilliez.antoine@gmail.com>
2019-09-10 09:58:09 +0200

    Preparation for release.

--- scripts/file_manager.py
+++ scripts/file_manager.py
@@ -1,5 +1,6 @@
-import datetime
-import logging
+""" This file supply a bunch of tools to facilitate file manaement, especially for file included in the ./data folder.
+"""
+
 import pickle
 import time
 
@@ -7,7 +8,7 @@ import pandas as pd
 from skimage import io
 import sys
 import os
-from ast import literal_eval
+
 
 FILE_NAME_TEMPLATE = "C10DsRedlessxYw_emb{}_Center_Out.tif"
 # DATA_PATH = "C:\\Users\\Antoine\\PycharmProjects\\Protein_image_processing\\data"
@@ -26,12 +27,29 @@ def get_embryos():
 
 
 def get_tiff_file(embryo):
+    """ Open a tiff file from the id of the embryo.
+
+    :param embryo: int
+        Key or id used to identify the embryo in the file name. Template use for file naming must be : C10DsRedlessxYw_emb{embryo id}_Center_Out.tif
+    :return: tiff: ndarray
+        4 dimensional array including all images and all channels
+    """
     file_path = DATA_PATH + "\\embryos\\" + FILE_NAME_TEMPLATE.format(embryo)
     tiff = io.imread(file_path)
     return tiff
 
 
 def save_results(df, file_name=None, timestamped=False):
+    """ Save pandas.DataFrame results in csv file.
+
+    :param df: pandas.DataFrame
+        Input DataFrame to export in .csv format
+    :param file_name: str
+        Name of the file
+    :param timestamped: bool
+        If yes, add a timestamp at the end of the file name
+    """
+
     directory = DATA_PATH + "\\results\\"
     file_name = format_file_name(file_name, directory, timestamped, ".csv")
     if isinstance(df, pd.DataFrame):
@@ -41,6 +59,13 @@ def save_results(df, file_name=None, timestamped=False):
 
 
 def get_test_features(file_name="Features_cells_C10DsRedlessxYw_emb11_Center_Out.tif.csv"):
+    """ Return DataFrame from existing .csv file in the data directory.
+
+    :param file_name: str
+        file name. Can be setted with or without the csv extention.
+    :return: df: pandas.DataFrame
+        the data.
+    """
     if file_name[:-4]!=".csv":
         file_name+=".csv"
 
@@ -53,12 +78,16 @@ def get_test_features(file_name="Features_cells_C10DsRedlessxYw_emb11_Center_Out
     return df
 
 
-def save_timestamped_results(df, title):
-    file_name = title + "{}".format(datetime.now()) + ".csv"
-    save_results(df, file_name=file_name)
-
-
 def save_as_pickle(data, file_name=None, timestamped=False):
+    """ Save python object in pickle format. Object can be of any pythonic type. Pickle file will be saved in ./data/variables directory
+
+    :param data: Object
+        Data to save
+    :param file_name: str
+        file name
+    :param timestamped: bool
+        If yes, add a timestamp at the end of the file name
+    """
     directory = DATA_PATH + "\\variables\\"
     file_name = format_file_name(file_name, directory, timestamped, ".pkl")
     try:
@@ -70,7 +99,15 @@ def save_as_pickle(data, file_name=None, timestamped=False):
 
 
 def load_pickle(file_name):
+    """ Load pickle file from ./data/variables directiory.
+
+    :param file_name:
+        Name of the file
+    :return: data: Object
+        Python object stored in the pickle file
+    """
     directory = DATA_PATH + "\\variables\\"
+    file_name = format_file_name(file_name, directory, False, '.pkl')
     file_name = directory + file_name
     with open(file_name, 'rb') as f:
         data = pickle.load(f)
@@ -78,6 +115,20 @@ def load_pickle(file_name):
 
 
 def format_file_name(file_name, directory, timestamped, extension):
+    """ Util that facilitate file name management by generating a standard and complete file name from the input conditions.
+
+    :param file_name: str
+        Input file name
+    :param directory: str
+        Directory where the file is stored
+    :param timestamped: bool
+        If yes, add timestamp at the end of the file name and prior to its extension
+    :param extension: str
+        Extention of the file. Preferably '.csv' or '.pkl'.
+    :return: file_name: str
+        Formated file name.
+    """
+
     if file_name is not None and file_name[-4:] == extension:
         # remove extension
         file_name = file_name[:-4]
@@ -92,6 +143,16 @@ def format_file_name(file_name, directory, timestamped, extension):
 
 
 def exist(file_name):
+    """ Check if a file exist in any of the subdirectories of ./data
+
+    :param file_name: str
+        File name
+    :return:
+        if file exist: str
+            Complete path to access the file.
+        else : bool
+            False
+    """
     paths = [DATA_PATH]
     for dir in get_data_subdirs():
         paths.append(DATA_PATH+"\\"+dir)
@@ -102,6 +163,10 @@ def exist(file_name):
 
 
 def get_data_subdirs():
+    """ Returns all subdirectories of ./data
+    :return: array
+        list of directories
+    """
     dirs = []
     for x in os.listdir(DATA_PATH):
         if os.path.isdir(DATA_PATH+"\\"+x):
