b28edc15105740f1f426036e3104dca2a6dd989a
Galsor <meilliez.antoine@gmail.com>
2019-09-29 17:49:01 +0200

    Default filter to "auto"

--- scripts/file_processing.py
+++ scripts/file_processing.py
@@ -9,8 +9,8 @@ import logging
 
 import scripts.file_manager as fm
 from scripts.preprocessing import blob_extraction, label_filter, label_filter_blobs, find_cells_contours, \
-    label_filter_contours
-from scripts.region import region_properties, overlaped_regions
+    label_filter_contours, define_filter_value
+from scripts.region import region_properties, overlapped_regions
 from scripts.region_3d import RegionFrame
 
 
@@ -26,7 +26,7 @@ PROPERTIES = ['area',
               'convex_area']
 
 
-def extract_regions(tiff, channel=0, min_area=2, filter=0.10, cells=None, mode='region'):
+def extract_regions(tiff, channel=0, min_area=2, filter="auto", cells=None, mode='region'):
     """
     Extract regions3D from each layers of a tiff file.
 
@@ -55,17 +55,26 @@ def extract_regions(tiff, channel=0, min_area=2, filter=0.10, cells=None, mode='
         ch = tiff[:, :, :, channel]
     except Exception as e:
         raise e
+
+    # Workaround to remove after refactoring of label filtering
+    if filter == "auto":
+        filter = define_filter_value(ch, filter = filter)
+
     if mode == 'blob':
+        #Todo : refactor and create a step safe mode with pickle saving. Clean pickles after complete processing of the pipeline.
         if fm.exist("data_test_blob_detect.pkl"):
             blobs, rscl_img = fm.load_pickle("data_test_blob_detect.pkl")
         else:
+            logging.info(" === BLOB DETECTION ===")
             blobs, rscl_img = blob_extraction(ch)
 
     init = True
+    logging.info("=== REGION EXTRACTION ===")
     for layer, img in enumerate(ch):
         logging.info("_" * 80)
         logging.info("Layer {}".format(layer))
         logging.info("_" * 80)
+        logging.info("Extract regions")
         if mode == 'region':
             df_properties = region_properties(label_filter(img, filter=filter)[0], img, properties=PROPERTIES,
                                               min_area=min_area, cells=cells)
@@ -75,14 +84,16 @@ def extract_regions(tiff, channel=0, min_area=2, filter=0.10, cells=None, mode='
             df_properties = region_properties(label_filter_blobs(img, layer_blobs, filter=filter)[0], img, properties=PROPERTIES,
                                               min_area=min_area, cells=cells)
         # Previously test if 'regions' existing. Region has been replaced by df_properties
+        logging.info("Add results to the RegionFrame")
         if init:
             rf = RegionFrame(df_properties)
             init = False
             prev_img = img
         elif not init:
             region_dict = rf.get_regions_in_last_layer()
-            matched_regions, new_regions = overlaped_regions(img, df_properties, prev_img, region_dict,
-                                                                         filter=filter)
+            logging.info("Look for overlapped regions")
+            matched_regions, new_regions = overlapped_regions(img, df_properties, prev_img, region_dict,
+                                                              filter=filter)
             existing_regions_map = rf.enrich_region3D(matched_regions)
             if not new_regions.empty:
                 new_regions_map = rf.populate_region3D(new_regions)
@@ -93,6 +104,7 @@ def extract_regions(tiff, channel=0, min_area=2, filter=0.10, cells=None, mode='
 
     logging.info("Total amount of regions detected {}".format(rf.get_amount_of_regions3D()))
     return rf
+
 
 def extract_cells(tiff):
     """Generate labeled image of each cells in the tiff file.
@@ -110,36 +122,19 @@ def extract_cells(tiff):
     label_img, bin = label_filter_contours(contours, ch3[0].shape)
     return label_img
 
-def extract_region_with_cells(tiff, channel=0, min_area=2, filter=0.10, mode='region'):
+
+def extract_region_with_cells(tiff, channel=0, min_area=2, filter="auto", mode='region'):
     label_cells = extract_cells(tiff)
     rf = extract_regions(tiff, cells=label_cells, channel=channel, min_area=min_area, filter=filter, mode=mode)
     return rf
 
-
-
-def test_pipeline():
-    # function
-    file_path_template = "C:\\Users\\Antoine\\PycharmProjects\\Protein_image_processing\\data\\embryos\\" \
-                         "C10DsRedlessxYw_emb{}_Center_Out.tif"
-    EMBRYOS = {1: (77, 24221), 7: (82, 23002), 8: (71, 15262), 10: (92, 23074)}
-
-    try:
-        results = {}
-        for embryo in EMBRYOS.keys():
-            file_path = file_path_template.format(embryo)
-            results[embryo] = extract_regions(file_path).get_amount_of_regions3D()
-        for embryo, r in results.items():
-            expected = EMBRYOS[embryo][0] + EMBRYOS[embryo][1]
-            logging.info(" {} (expected {} ) proteins detected for embryo {}").format(r, expected, embryo)
-
-    except Exception as e:
-        raise e
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     embs = fm.get_embryos()
     for emb in embs :
         tiff = fm.get_tiff_file(emb)
-        rf = extract_region_with_cells(tiff,filter = 100)
+        rf = extract_region_with_cells(tiff)
         df = rf.extract_features()
-        fm.save_results(df, file_name="features_extraction_emb{}_".format(emb), timestamped=True)
+        print("Regions detected : {}".format(len(df)))
+        fm.save_results(df, file_name="features_extraction_auto_emb{}_".format(emb), timestamped=True)
